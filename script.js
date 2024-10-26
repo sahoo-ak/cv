4117c085e3ef11352023d6736a35dc0cd2a23e71
sahoo-ak <aloka.ku.sahoo@gmail.com>
2024-10-26 23:34:28 +0530

    update details and modern style

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById('toggle-nav');
+const navMenu = document.getElementById('nav-menu');
+
+toggleButton.addEventListener('click', () => {
+  navMenu.classList.toggle('open');
+});
